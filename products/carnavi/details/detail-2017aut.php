12deac43bb3ac54f2a134e9b4f58a7f5b714ed59
ZeroShiki <ds.nakazato@gmail.com>
2018-11-09 13:29:27 +0900

    Added new HTML

--- products/carnavi/details/detail-2017aut.php
+++ products/carnavi/details/detail-2017aut.php
@@ -1,10 +1,10 @@
 <h3 class="mb-4 pb-2 border-bottom"><strong>全国1,338都市の詳細地図</strong></h3>
-	<div class="row mb-5">
-		<div class="col-md-6">
-			<p>日本全国1,338都市の詳細地図を収録しています。建物名や建物の形状、高さ、細かい道路を詳しく表示するので、初めて通る道路でも、カーナビの画面上で確認することができます。</p>
-			<p class="small">（※すべての都市を100%網羅しているわけではありません。）</p>
-		</div>
-		<div class="col-md-6">
-			<img src="http://localhost/archive_nplace/img/carnavi/2017aut_map_detail_100m.jpg" alt="詳細地図" class="img-fluid">
-		</div>
+<div class="row mb-5">
+	<div class="col-md-6">
+		<p>日本全国1,338都市の詳細地図を収録しています。建物名や建物の形状、高さ、細かい道路を詳しく表示するので、初めて通る道路でも、カーナビの画面上で確認することができます。</p>
+		<p class="small">（※すべての都市を100%網羅しているわけではありません。）</p>
 	</div>
+	<div class="col-md-6">
+		<img src="http://localhost/archive_nplace/img/carnavi/2017aut_map_detail_100m.jpg" alt="詳細地図" class="img-fluid">
+	</div>
+</div>
---
+++ products/carnavi/details/map-2016aut.php
@@ -0,0 +1,12 @@
+<h3 class="mb-4 pb-2 border-bottom"><strong>2016年地図データ秋版を収録</strong></h3>
+<div class="row mb-5">
+	<div class="col-md-6">
+		<ul class="pl-3">
+			<li>2016年7月1日までの地図データを採用しています。（一部データを除きます。）</li>
+			<li>住所登録データ 約3,913万件、施設（名称）検索データ 約640万件を収録。多くのデータにアクセスできます。さらに、２画面表示の際の分岐イラストも大幅に増加し、より走行しやすくなりました。</li>
+		</ul>
+	</div>
+	<div class="col-md-6">
+		<img src="http://localhost/archive_nplace/img/carnavi/rurubu_id.jpg" alt="2016年7月1日までに開通した道路例" class="img-fluid">
+	</div>
+</div>
