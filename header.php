c59e92d525fa6f586a90a8f62fd59f9c16340f1b
ZeroShiki <ds.nakazato@gmail.com>
2018-11-27 16:52:59 +0900

    updated HTML, CSS and PHP 2018-11-27

--- header.php
+++ header.php
@@ -1,29 +1,43 @@
-<header>	  	 
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	 	<div class="container">
-	 		<a class="navbar-brand" href="http://localhost/archive_nplace/"><img src="http://localhost/archive_nplace/img/common/site_id_2x_white.png" height="40" alt="ロゴ"></a>
-	 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#切り替え表示されるコンテンツ名" aria-controls="切り替え表示されるコンテンツ名" aria-expanded="false" aria-label="ナビゲーション切り替え">
-	 				<span class="navbar-toggler-icon"></span>
-	 			</button>
-		 
-	 			<div class="collapse navbar-collapse" id="切り替え表示されるコンテンツ名">
-	 				<ul class="navbar-nav mr-auto">
-	 					<li class="nav-item dropdown">
-	 						<a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">製品情報</a>
-	 						<div class="dropdown-menu" aria-labelledby="dropdown01">
-	 							<a class="dropdown-item" href="http://localhost/archive_nplace/products/carnavi/index.html">カーナビゲーション</a>
-	 							<a class="dropdown-item" href="#">ドライブレコーダー</a>
-	 						</div>
-	 					</li>
-	 					<li class="navbar-nav">
-	 						<a class="nav-link" href="#">サポート</a>	
-	 					</li>
-	 				</ul>
-	 			<form class="form-inline my-2 my-lg-0">
-					<input class="form-control mr-sm-2" type="search" placeholder="検索" aria-label="検索">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
-				</form>
-			</div>
-		</div>
-	</nav>
- </header>
+<!doctype html>
+<html lang="ja"><head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="http://localhost/archive_nplace/css/style.css">
+		
+		<!-- Javascript -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script type='text/javascript' src="http://localhost/archive_nplace/js/breadcrumb.js"></script>
+		
+		<title>エンプレイス|<?php echo title; ?></title>
+	</head>
+	<body>
+		<header class="sticky-top"> 
+				<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffdf44;">
+					<a class="navbar-brand" href="http://localhost/archive_nplace/"><img src="http://localhost/archive_nplace/img/common/site_id_2x.png" height="40" alt="logo"></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#contents-name" aria-controls="contents-name" aria-expanded="false" aria-label="ナビゲーション切り替え">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="contents-name">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">製品情報</a>
+								<div class="dropdown-menu" aria-labelledby="dropdown01">
+									<a class="dropdown-item" href="http://localhost/archive_nplace/products/carnavi/index.html">カーナビゲーション</a>
+									<a class="dropdown-item" href="#">ドライブレコーダー</a>
+								</div>
+							</li>
+							<li class="navbar-nav">
+								<a class="nav-link" href="#">サポート</a>	
+							</li>
+						</ul>
+						<form class="form-inline my-2 my-lg-0">
+							<input class="form-control mr-sm-2" type="search" placeholder="検索" aria-label="検索">
+							<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">検索</button>
+						</form>
+					</div>
+				</nav>
+			 </header>
