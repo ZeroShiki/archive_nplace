af9b9f4d4e05d9d8abe29b7366ea808276c6a363
ZeroShiki <ds.nakazato@gmail.com>
2019-04-22 16:45:01 +0900

    modified

--- footer.php
+++ footer.php
@@ -35,5 +35,10 @@
 			<span class="small"><a class="text-muted" href="https://www.youtube.com/user/NPLACEchannel" target="_blank"><i class="fab fa-youtube"></i>Youtube</a></span>
 		</div>
 	</footer>
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
--- header.php
+++ header.php
@@ -22,6 +22,7 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 		<!-- Javascript -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 		<script src="http://localhost/archive_nplace/js/jquery-3.4.0.min.js"></script>
 		<script src="http://localhost/archive_nplace/js/breadcrumb.js"></script>
 		<script src="http://localhost/archive_nplace/js/scroll.js"></script>
