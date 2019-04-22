475685b6313054bf8611d61674bfa2dd50b3c31c
ZeroShiki <ds.nakazato@gmail.com>
2019-04-22 16:37:36 +0900

    modified css

--- header.php
+++ header.php
@@ -16,15 +16,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="http://archive.nplace.jp/css/style.css">
+		<link rel="stylesheet" href="http://localhost/archive_nplace/css/style.css">
 		
 		<!-- FontAwesome CSS -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 		<!-- Javascript -->
-		<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script type='text/javascript' src="http://archive.nplace.jp/js/breadcrumb.js"></script>
-		<script type='text/javascript' src="http://archive.nplace.jp/js/scroll.js"></script>
+		<script src="http://localhost/archive_nplace/js/jquery-3.4.0.min.js"></script>
+		<script src="http://localhost/archive_nplace/js/breadcrumb.js"></script>
+		<script src="http://localhost/archive_nplace/js/scroll.js"></script>
 
 		<title>エンプレイス|<?php echo title; ?></title>
 	</head>
@@ -32,18 +32,16 @@
 		<header class="sticky-top"> 
 			<nav class="navbar navbar-expand-sm navbar-light" style="background-color: #ffdf44;">
 				<div class="container">
-
-
 					<div class="collapse navbar-collapse" id="contents-name">
 						<ul class="navbar-nav mr-auto">
 							<li class="navbar-nav">
-								<a class="nav-link" href="http://archive.nplace.jp/">生産完了品トップ</a>	
+								<a class="nav-link" href="http://localhost/archive_nplace/">生産完了品トップ</a>	
 							</li>
 							<li class="nav-item dropdown">
-								<a class="nav-link dropdown-toggle" href="http://archive.nplace.jp/" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">製品情報</a>
+								<a class="nav-link dropdown-toggle" href="http://localhost/archive_nplace/" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">製品情報</a>
 								<div class="dropdown-menu" aria-labelledby="dropdown01">
-									<a class="dropdown-item" href="http://archive.nplace.jp/products/carnavi/index.php">カーナビゲーション</a>
-									<a class="dropdown-item" href="http://archive.nplace.jp/products/drive_recorder/index.php">ドライブレコーダー</a>
+									<a class="dropdown-item" href="http://localhost/archive_nplace/products/carnavi/index.php">カーナビゲーション</a>
+									<a class="dropdown-item" href="http://localhost/archive_nplace/products/drive_recorder/index.php">ドライブレコーダー</a>
 								</div>
 							</li>
 							<li class="navbar-nav">
@@ -51,7 +49,7 @@
 							</li>
 						</ul>
 					</div>
-					<a class="navbar-brand mr-0" href="http://nplace.jp/"><img src="http://archive.nplace.jp/img/common/site_id_2x.png" height="40" alt="logo"></a>
+					<a class="navbar-brand mr-0" href="http://nplace.jp/"><img src="http://localhost/archive_nplace/img/common/site_id_2x.png" height="40" alt="logo"></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#contents-name" aria-controls="contents-name" aria-expanded="false" aria-label="ナビゲーション切り替え">
 						<span class="navbar-toggler-icon"></span>
 					</button>
